13ea02e875d5901a917d244f44ce65b29bbc6f21
Hei <lamyuenhei@gmail.com>
2012-10-31 02:31:50 +0800

    Added uniqueness analysis module

---
+++ uniqueness/src/main/java/com/duality/uniqueness/SelfUniqueness.java
@@ -0,0 +1,112 @@
+package com.duality.uniqueness;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import opennlp.tools.tokenize.SimpleTokenizer;
+
+public class SelfUniqueness {
+
+	private final int tokenNum;
+	private final double score;
+	private final double groupNum;
+	private final double avgLength;
+
+	/**
+	 * @param filename File to analysis
+	 * @param n Number of token as a group
+	 * @throws IOException
+	 */
+	public SelfUniqueness(final String filename, final int n) throws IOException {
+		this.tokenNum = n;
+
+		final FileReader fileReader = new FileReader(filename);
+		final BufferedReader reader = new BufferedReader(fileReader);
+
+		final SimpleTokenizer tokenizer = SimpleTokenizer.INSTANCE;
+		final Set<List<String>> coreSet = new HashSet<List<String>>();
+
+		double tokenNum = 0;
+		int lineNum = 0;
+		int groupNum = 0;
+		String line;
+		while ((line = reader.readLine()) != null) {
+			final String[] tokens = tokenizer.tokenize(line);
+			final int maxIndex = tokens.length - n;
+			tokenNum += tokens.length;
+
+			for (int i = 0; i < maxIndex; i++) {
+				final String[] group = new String[n];
+				System.arraycopy(tokens, i, group, 0, n);
+				coreSet.add(Arrays.asList(group));
+				groupNum++;
+			}
+			lineNum++;
+		}
+
+		reader.close();
+
+		score = (double) coreSet.size() / groupNum;
+		this.groupNum = groupNum;
+		this.avgLength = tokenNum / lineNum;
+	}
+
+	public double getScore() {
+		return score;
+	}
+
+	public int getTokenNumInGroup() {
+		return tokenNum;
+	}
+
+	public double getGroupNum() {
+		return groupNum;
+	}
+
+	public double getAvgLength() {
+		return avgLength;
+	}
+
+	public static void main(final String[] args) throws IOException {
+		if (args.length != 3) {
+			System.out.println("Invalid args.\nUsage: selfUniquness [dataSetDir] [tokenNumInGroup] [output]");
+			return;
+		}
+
+		final String dataSetDir = args[0];
+		final File dataSet = new File(dataSetDir);
+		if (!dataSet.isDirectory()) {
+			System.out.println(dataSetDir + " is not a directory!");
+			return;
+		}
+
+		final String tokenNumStr = args[1];
+		final int tokenNum;
+		try {
+			tokenNum = Integer.parseInt(tokenNumStr);
+		} catch (final NumberFormatException e) {
+			System.out.println(tokenNumStr + " is not a number!");
+			return;
+		}
+
+		final String[] filenames = dataSet.list();
+		final FileWriter fileWriter = new FileWriter(args[2]);
+		fileWriter.write("filename, score, groupNum, avgLength\n");
+
+		for (final String filename : filenames) {
+			final SelfUniqueness uniqueness = new SelfUniqueness(dataSetDir + "/" + filename, tokenNum);
+			final double score = uniqueness.getScore();
+			final double groupNum = uniqueness.getGroupNum();
+			final double avgLength = uniqueness.getAvgLength();
+			fileWriter.write(filename + ", " + score + ", " + groupNum + ", " + avgLength + "\n");
+		}
+		fileWriter.close();
+	}
+}
---
+++ uniqueness/src/main/java/com/duality/uniqueness/Uniqueness.java
@@ -0,0 +1,103 @@
+package com.duality.uniqueness;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import opennlp.tools.tokenize.SimpleTokenizer;
+
+public class Uniqueness {
+
+	private final Set<List<String>> corpus;
+	private final int n;
+
+	public Uniqueness(final String filename, final int n) throws IOException {
+		this.n = n;
+
+		final FileReader fileReader = new FileReader(filename);
+		final BufferedReader reader = new BufferedReader(fileReader);
+
+		final SimpleTokenizer tokenizer = SimpleTokenizer.INSTANCE;
+		corpus = new HashSet<List<String>>();
+
+		String line;
+		while ((line = reader.readLine()) != null) {
+			final String[] tokens = tokenizer.tokenize(line);
+			final int maxIndex = tokens.length - n;
+
+			for (int i = 0; i < maxIndex; i++) {
+				final String[] group = new String[n];
+				System.arraycopy(tokens, i, group, 0, n);
+				corpus.add(Arrays.asList(group));
+			}
+		}
+
+		reader.close();
+	}
+
+	public double score(final String filename) throws IOException {
+		final FileReader fileReader = new FileReader(filename);
+		final BufferedReader reader = new BufferedReader(fileReader);
+
+		final SimpleTokenizer tokenizer = SimpleTokenizer.INSTANCE;
+
+		int repeatedCount = 0;
+		int size = 0;
+		String line;
+		while ((line = reader.readLine()) != null) {
+			final String[] tokens = tokenizer.tokenize(line);
+			final int maxIndex = tokens.length - n;
+
+			for (int i = 0; i < maxIndex; i++) {
+				final String[] group = new String[n];
+				System.arraycopy(tokens, i, group, 0, n);
+				final boolean repeated = corpus.contains(Arrays.asList(group));
+				if (repeated) {
+					repeatedCount++;
+				}
+
+				size++;
+			}
+		}
+
+		reader.close();
+
+		final double uniquness = (double) repeatedCount / (double) size;
+		return uniquness;
+	}
+
+	public static void main(final String[] args) throws IOException {
+		if (args.length != 3) {
+			System.out.println("Invalid args.\nUsage: uniquness [trainingSet] [dataSetDir] [tokenNumInGroup]");
+			return;
+		}
+
+		final String dataSetDir = args[1];
+		final File dataSet = new File(dataSetDir);
+		if (!dataSet.isDirectory()) {
+			System.out.println(dataSetDir + " is not a directory!");
+			return;
+		}
+
+		final String tokenNumStr = args[2];
+		final int tokenNum;
+		try {
+			tokenNum = Integer.parseInt(tokenNumStr);
+		} catch (final NumberFormatException e) {
+			System.out.println(tokenNumStr + " is not a number!");
+			return;
+		}
+
+		final Uniqueness uniqueness = new Uniqueness(args[0], tokenNum);
+		final String[] filenames = dataSet.list();
+		for (final String filename : filenames) {
+			final double score = uniqueness.score(dataSetDir + "/" + filename);
+			System.out.println(filename + ": " + score);
+		}
+	}
+}
