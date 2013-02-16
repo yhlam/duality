14acace9963edcbe7227ffac9d04022736800f47
kenny2tam <kenny2tam@gmail.com>
2013-02-16 18:44:09 +0800

    1st Client GUI. No asmack XMPP

---
+++ client/src/com/duality/client/About_Activity.java
@@ -0,0 +1,23 @@
+package com.duality.client;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class About_Activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about_layout);
+
+	}	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main_layout, menu);
+		return true;
+	}
+
+}
