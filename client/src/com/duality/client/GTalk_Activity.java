8ae27983385b7b34a83126c57619b19b209f8795
kenny2tam <kenny2tam@gmail.com>
2013-02-17 22:51:52 +0800

    GUI can send msg to GTalk now! But cannot see or receive any message yet

--- client/src/com/duality/client/GTalk_Activity.java
+++ client/src/com/duality/client/GTalk_Activity.java
@@ -1,8 +1,11 @@
 package com.duality.client;
 
+import java.util.ArrayList;
+
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Presence;
 
 import android.app.Activity;
@@ -12,7 +15,10 @@ import android.os.HandlerThread;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class GTalk_Activity extends Activity {
@@ -20,11 +26,20 @@ public class GTalk_Activity extends Activity {
 	private HandlerThread mThread;
 	private XMPPConnection xmpp;
 	private ConnectionConfiguration connect;
-	
+
+	private EditText receiver;
+	private EditText message;
+	private ListView sentMessage;
+	private ArrayList<String> messages = new ArrayList();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_gtalk_layout);
+
+		receiver = (EditText) this.findViewById(R.id.receiver);
+		message = (EditText) this.findViewById(R.id.message);
+		sentMessage = (ListView)this.findViewById(R.id.listMessages);
 
 		Button setting = (Button) findViewById(R.id.setting);
 		setting.setOnClickListener(new Button.OnClickListener(){
@@ -39,6 +54,35 @@ public class GTalk_Activity extends Activity {
 			}
 
 		});
+
+		Button send = (Button) this.findViewById(R.id.send);
+		send.setOnClickListener(new Button.OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//String to = receiver.getText().toString();
+				//String text = message.getText().toString();
+				String to = "lamyuenhei@gmail.com";
+				String text = "Testing from Duality Messanger";
+				Message msg = new Message(to, Message.Type.chat);
+				msg.setBody(text);
+				try{
+					xmpp.sendPacket(msg);
+					messages.add(xmpp.getUser() + ":");
+					messages.add(text);
+					//setListAdapter();
+				}catch (Exception e){
+					e.printStackTrace();
+				}
+			}
+
+		});
+	}
+
+	private void setListAdapter() {
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.multi_line_list_item,messages); 
+		sentMessage.setAdapter(adapter);
 	}
 
 	@Override
