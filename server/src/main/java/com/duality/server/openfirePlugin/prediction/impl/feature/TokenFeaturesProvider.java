71ea188e0fc4bb6ed9cdf11a0855fbddfc32bc9e
Hei <lamyuenhei@gmail.com>
2013-03-29 00:58:41 +0800

    Added support of tokenizing Chinese

--- server/src/main/java/com/duality/server/openfirePlugin/prediction/impl/feature/TokenFeaturesProvider.java
+++ server/src/main/java/com/duality/server/openfirePlugin/prediction/impl/feature/TokenFeaturesProvider.java
@@ -1,29 +1,40 @@
 package com.duality.server.openfirePlugin.prediction.impl.feature;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.List;
 
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.tartarus.snowball.ext.EnglishStemmer;
-
-import opennlp.tools.tokenize.SimpleTokenizer;
+import org.wltea.analyzer.lucene.IKAnalyzer;
 
 import com.duality.server.openfirePlugin.dataTier.HistoryEntry;
 import com.duality.server.openfirePlugin.prediction.impl.TfIdfUtils;
 import com.duality.server.openfirePlugin.prediction.impl.feature.AtomicFeature.FeatureType;
+import com.google.common.collect.Lists;
 
 public class TokenFeaturesProvider implements AtomicFeaturesProvider {
-	private static final SimpleTokenizer TOKENIZER = SimpleTokenizer.INSTANCE;
-
-	public static String[] extractTokens(final String message) {
+	public static List<String> extractTokens(final String message) {
+		final StringReader reader = new StringReader(message);
+		final IKAnalyzer ikAnalyzer = new IKAnalyzer();
+		final TokenStream tokenStream = ikAnalyzer.tokenStream("", reader);
+		final CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
 		final EnglishStemmer stemmer = new EnglishStemmer();
-		final String[] tokens = TOKENIZER.tokenize(message);
-		for (int i = 0; i < tokens.length; i++) {
-			final String token = tokens[i];
-			final String lowerCase = token.toLowerCase();
-			stemmer.setCurrent(lowerCase);
-			stemmer.stem();
-			final String stemmed = stemmer.getCurrent();
-			tokens[i] = stemmed;
+		final List<String> tokens = Lists.newLinkedList();
+		try {
+			while (tokenStream.incrementToken()) {
+				final String term = charTermAttribute.toString();
+				stemmer.setCurrent(term);
+				stemmer.stem();
+				final String stemmed = stemmer.getCurrent();
+				tokens.add(stemmed);
+			}
 		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		ikAnalyzer.close();
 
 		return tokens;
 	}
@@ -31,7 +42,7 @@ public class TokenFeaturesProvider implements AtomicFeaturesProvider {
 	@Override
 	public void constructFeatures(final HistoryEntry history, final List<AtomicFeature<?>> features) {
 		final String message = history.getMessage();
-		final String[] tokens = extractTokens(message);
+		final List<String> tokens = extractTokens(message);
 		for (final String token : tokens) {
 			TfIdfUtils.addAtomicFeature(features, FeatureType.TOKEN, token);
 		}
