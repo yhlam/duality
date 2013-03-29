e393f504d08ecabf6248ffca28869102c0738c14
Hei <lamyuenhei@gmail.com>
2013-03-29 17:16:15 +0800

    Implemented HistoryDatabaseAdapter

--- server/src/main/java/com/duality/server/openfirePlugin/HistoryPacketInterceptor.java
+++ server/src/main/java/com/duality/server/openfirePlugin/HistoryPacketInterceptor.java
@@ -1,10 +1,21 @@
 package com.duality.server.openfirePlugin;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+import org.dom4j.Element;
+import org.jivesoftware.database.DbConnectionManager;
 import org.jivesoftware.openfire.interceptor.PacketInterceptor;
 import org.jivesoftware.openfire.interceptor.PacketRejectedException;
 import org.jivesoftware.openfire.session.Session;
 import org.xmpp.packet.Message;
 import org.xmpp.packet.Packet;
+
+import com.duality.server.openfirePlugin.dataTier.HistoryDatabaseAdapter;
+import com.duality.server.openfirePlugin.dataTier.HistoryEntry;
 
 public class HistoryPacketInterceptor implements PacketInterceptor {
 	private static final HistoryPacketInterceptor INSTANCE = new HistoryPacketInterceptor();
@@ -15,7 +26,8 @@ public class HistoryPacketInterceptor implements PacketInterceptor {
 
 	@Override
 	public void interceptPacket(Packet packet, Session session, boolean incoming, boolean processed) throws PacketRejectedException {
-		if(incoming && packet instanceof Message) {
+		
+		if(processed && incoming && packet instanceof Message) {
 			final Message message = (Message) packet;
 			final String body = message.getBody();
 			if(body != null && !"".equals(body)) {
@@ -26,5 +38,46 @@ public class HistoryPacketInterceptor implements PacketInterceptor {
 
 	private void recordMessage(Message message) {
 		// TODO (Terry): record the package to database by using HistoryDatabaseAdapter 
+		final  HistoryDatabaseAdapter historyDatabaseAdapter = HistoryDatabaseAdapter.singleton();
+		
+		long id = 0; //default value; should be MAX(ID)+1 from database 
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			con= DbConnectionManager.getConnection();
+			pstmt = con.prepareStatement("SELECT MAX(ID) FROM DUALITY");
+			rs = pstmt.executeQuery();
+			if (rs.next()){
+				id = rs.getInt(1) + 1;
+			}
+			else{ //empty table
+				id = 0;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			DbConnectionManager.closeConnection(rs,pstmt,con);
+		}
+		
+		
+		
+		Element contextDataElement = message.getChildElement("contextData", "duality");
+		//double senderlatitude = Double.parseDouble(contextDataElement.element("senderlatitude").getTextTrim());
+		//double senderlongtitude = Double.parseDouble(contextDataElement.element("senderlongtitude").getTextTrim());
+		//double receiverlatitude = Double.parseDouble(contextDataElement.element("receiverlatitude").getTextTrim());
+		//double receiverlongtitude = Double.parseDouble(contextDataElement.element("receiverlongtitude").getTextTrim());
+		double senderlatitude = 12.1; //hard code
+		double senderlongtitude = 13.1; //hard code
+		double receiverlatitude = 14; //hard code
+		double receiverlongtitude =15; //hard code
+		String sender = message.getFrom().toString();
+		String receiver = message.getTo().toString();
+		String messageBody = message.getBody();
+		HistoryEntry historyEntry = new HistoryEntry(id, sender,receiver, new Date(),messageBody, 
+				senderlatitude,	senderlongtitude,receiverlatitude,receiverlongtitude);
+		
+		historyDatabaseAdapter.addHistory(historyEntry);
 	}
 }
--- server/src/main/java/com/duality/server/openfirePlugin/dataTier/HistoryDatabaseAdapter.java
+++ server/src/main/java/com/duality/server/openfirePlugin/dataTier/HistoryDatabaseAdapter.java
@@ -1,6 +1,14 @@
 package com.duality.server.openfirePlugin.dataTier;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import org.jivesoftware.database.DbConnectionManager;
 
 /**
  * HistoryDatabaseAdapter is responsible for read and write the History database
@@ -23,29 +31,111 @@ public class HistoryDatabaseAdapter {
 	 * @return All chat history stored in database
 	 */
 	public List<HistoryEntry> getAllHistory() {
-		// TODO (Terry): Read the history from database or cache
-		return null;
+		List<HistoryEntry> result = new ArrayList<HistoryEntry>();
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			con= DbConnectionManager.getConnection();
+			pstmt = con.prepareStatement("SELECT * FROM duality");
+			rs = pstmt.executeQuery();
+			while (rs.next()){
+				//make the list of HistoryEntry
+				
+				long id=rs.getLong("ID");
+				String sender = rs.getString("SENDER");
+				String receiver=rs.getString("RECEIVER");
+				Date time = new Date (rs.getLong("TIME"));
+				String message = rs.getString("MESSAGE");
+				double senderlatitude = rs.getLong("SENDER_LATITUDE");
+				double senderlongitude = rs.getLong("SENDER_LONGITUDE");;
+				double receiverlatitude = rs.getLong("RECEIVER_LATITUDE");;
+				double receiverlongitude = rs.getLong("RECEIVER_LONGITUDE");;
+				HistoryEntry entry = new HistoryEntry(id,sender,receiver,time,message,senderlatitude,senderlongitude,receiverlatitude,receiverlongitude);
+				result.add(entry);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			DbConnectionManager.closeConnection(rs,pstmt,con);
+		}
+		
+		return result;
 	}
 
 	/**
 	 * Get History for a given ID
-	 * 
-	 * @param id
-	 *            ID of a history entry
+	 * @param id ID of a history entry
 	 * @return a HistoryEntry with the ID
 	 */
-	public HistoryEntry getHistoryById(final long id) {
-		// TODO (Terry): Get the corresponding ID from database or cache
+	public HistoryEntry getHistoryById(long id) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			con= DbConnectionManager.getConnection();
+			pstmt = con.prepareStatement("SELECT * FROM duality WHERE ID=" + id);
+			rs = pstmt.executeQuery();
+			if (rs.next()){
+				long entryId=rs.getLong("ID");
+				String sender = rs.getString("SENDER");
+				String receiver=rs.getString("RECEIVER");
+				Date time = new Date (rs.getLong("TIME"));
+				String message = rs.getString("MESSAGE");
+				double senderlatitude = rs.getLong("SENDER_LATITUDE");
+				double senderlongitude = rs.getLong("SENDER_LONGITUDE");;
+				double receiverlatitude = rs.getLong("RECEIVER_LATITUDE");;
+				double receiverlongitude = rs.getLong("RECEIVER_LONGITUDE");;
+				HistoryEntry entry = new HistoryEntry(id,sender,receiver,time,message,senderlatitude,senderlongitude,receiverlatitude,receiverlongitude);
+
+				return entry;
+			}
+			else{
+				return null;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			DbConnectionManager.closeConnection(rs,pstmt,con);
+		}
+		
+		
 		return null;
 	}
 
 	/**
 	 * Write a HistoryEntry to database
-	 * 
-	 * @param entry
-	 *            HistoryEntry to be written in database
+	 * @param entry HistoryEntry to be written in database
 	 */
-	public void addHistory(final HistoryEntry entry) {
-		// TODO (Terry): Write the history entry to database
+	public void addHistory(HistoryEntry entry) {
+		Connection con = null;
+		
+		long time = entry.getTime().getTime();
+		
+		try {
+			con= DbConnectionManager.getConnection();
+			String INSERT_SQL = "INSERT INTO duality (ID,SENDER,RECEIVER,TIME,MESSAGE,SENDER_LATITUDE,SENDER_LONGITUDE,RECEIVER_LATITUDE,RECEIVER_LONGITUDE) VALUES (?,?,?,?,?,?,?,?,?)";
+			PreparedStatement st = con.prepareStatement(INSERT_SQL);
+			st.setLong(1, entry.getId());
+			st.setString(2,entry.getSender());
+			st.setString(3,entry.getReceiver());
+			st.setLong(4,time);
+			st.setString(5,entry.getMessage());
+			st.setDouble(6, entry.getSenderlatitude());
+			st.setDouble(7, entry.getSenderLongtitude());
+			st.setDouble(8, entry.getReceiverlatitude());
+			st.setDouble(9, entry.getReceiverlatitude());
+			st.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally{
+			DbConnectionManager.closeConnection(con);
+		}
+
 	}
+	
 }
