28165be46178600f007005e8b33c2be12388372a
Hei <lamyuenhei@gmail.com>
2013-04-09 02:35:31 +0800

    Optimized query performance

--- server/src/main/java/com/duality/server/openfirePlugin/prediction/impl/TfIdfNgramPredictionEngine.java
+++ server/src/main/java/com/duality/server/openfirePlugin/prediction/impl/TfIdfNgramPredictionEngine.java
@@ -1,6 +1,8 @@
 package com.duality.server.openfirePlugin.prediction.impl;
 
-import java.util.HashSet;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -11,38 +13,95 @@ import com.duality.server.openfirePlugin.dataTier.HistoryEntry;
 import com.duality.server.openfirePlugin.prediction.FeatureKey;
 import com.duality.server.openfirePlugin.prediction.PredictionEngine;
 import com.duality.server.openfirePlugin.prediction.impl.feature.AtomicFeature;
-import com.duality.server.openfirePlugin.prediction.impl.feature.AtomicFeaturesManager;
 import com.duality.server.openfirePlugin.prediction.impl.feature.TfIdfKey;
 import com.duality.server.openfirePlugin.prediction.impl.feature.VectorSpaceFeatureKey;
-import com.duality.server.openfirePlugin.prediction.impl.store.FPStore;
+import com.duality.server.openfirePlugin.prediction.impl.store.TfIdfFeatureStore;
 import com.duality.server.openfirePlugin.prediction.impl.store.TfIdfStore;
+import com.duality.server.openfirePlugin.prediction.impl.store.TfIdfStore.TermFrequency;
+import com.google.common.base.Function;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Iterators;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.MinMaxPriorityQueue;
-import com.google.common.collect.Sets;
+import com.google.common.collect.Table;
 
 public class TfIdfNgramPredictionEngine extends PredictionEngine {
 	private static final int MAX_PREDICTION_NUM = 10;
 	private static final int NEXT_ENTRY_TIME_LIMIT = 60 * 60 * 1000; // 1 Hour
+	private static final Class<? extends ClosenessAggregator> AGGREGATOR = DotProductAggregator.class;
+
+	private static final ClosenessAggregator createAggregator() {
+		try {
+			final Constructor<? extends ClosenessAggregator> constructor = AGGREGATOR.getDeclaredConstructor();
+			constructor.setAccessible(true);
+			final ClosenessAggregator newInstance = constructor.newInstance();
+			return newInstance;
+		} catch (final InstantiationException e) {
+			e.printStackTrace();
+		} catch (final IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (final NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (final SecurityException e) {
+			e.printStackTrace();
+		} catch (final IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (final InvocationTargetException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
 
 	public TfIdfNgramPredictionEngine() {
 	}
-	
+
 	@Override
-	public List<String> getPredictions(HistoryEntry entry, String incompletedMessage) {
-		final Map<FeatureKey<?>, Object> context = extractFeatures(entry);
+	public List<String> getPredictions(final HistoryEntry entry, final String incompletedMessage) {
+		final TfIdfFeatureStore tfIdfFeatureStore = TfIdfFeatureStore.singleton();
+		final Map<FeatureKey<?>, Object> context = tfIdfFeatureStore.getFeatures(entry);
 		return getPredictions(context, incompletedMessage);
 	}
 
 	@Override
 	public List<String> getPredictions(final Map<FeatureKey<?>, Object> context, final String incompletedMessage) {
+		final TfIdfStore tfIdfStore = TfIdfStore.singleton();
+		final Set<FeatureKey<?>> contextFeatures = context.keySet();
+		final ClosenessAggregator aggregator = createAggregator();
+
 		final HistoryDatabaseAdapter historyDb = HistoryDatabaseAdapter.singleton();
-		final List<HistoryEntry> histories = historyDb.getAllHistory();
+		for (final FeatureKey<?> feature : contextFeatures) {
+			final TfIdfKey key = (TfIdfKey) feature;
+			final Set<AtomicFeature<?>> tokens = key.getTokens();
+			final Double tfIdf = key.getValue(context);
+			final double idf = tfIdfStore.getInvertedDocumentFrequency(tokens);
+			final double tfIdfIdf = tfIdf * idf;
+
+			final List<TermFrequency> tfs = tfIdfStore.getRelevantTermFrequencies(tokens);
+			for (final TermFrequency tf : tfs) {
+				final int id = tf.getDocumentId();
+
+				final HistoryEntry nextHistory = historyDb.nextHistoryEntry(id, NEXT_ENTRY_TIME_LIMIT);
+				if (nextHistory == null) {
+					continue;
+				}
+
+				final String message = nextHistory.getMessage();
+				if (incompletedMessage != null) {
+					if (!message.startsWith(incompletedMessage)) {
+						continue;
+					}
+				}
+
+				final double frequency = tf.getFrequency();
+				final double closeness = tfIdfIdf * frequency;
+				aggregator.offer(message, id, closeness);
+			}
+		}
+
 		final MinMaxPriorityQueue<MessageCloseness> queue = MinMaxPriorityQueue.maximumSize(MAX_PREDICTION_NUM).create();
-		for (final HistoryEntry history : histories) {
-			final Map<FeatureKey<?>, Object> features = extractFeatures(history);
-			final double closeness = dotProduct(context, features);
-			final MessageCloseness messageCloseness = new MessageCloseness(history, closeness);
+		for (final MessageCloseness messageCloseness : aggregator) {
 			queue.add(messageCloseness);
 		}
 
@@ -50,14 +109,8 @@ public class TfIdfNgramPredictionEngine extends PredictionEngine {
 
 		while (!queue.isEmpty()) {
 			final MessageCloseness closeness = queue.poll();
-			final HistoryEntry historyEntry = closeness.getHistoryEntry();
-			final HistoryEntry nextHistory = historyDb.nextHistoryEntry(historyEntry, NEXT_ENTRY_TIME_LIMIT);
-			if (nextHistory != null) {
-				final String message = nextHistory.getMessage();
-				if (incompletedMessage == null || message.startsWith(incompletedMessage)) {
-					predictions.add(message);
-				}
-			}
+			final String message = closeness.getMessage();
+			predictions.add(message);
 		}
 
 		if (predictions.isEmpty()) {
@@ -68,104 +121,17 @@ public class TfIdfNgramPredictionEngine extends PredictionEngine {
 		return predictions;
 	}
 
-	private Map<FeatureKey<?>, Object> extractFeatures(final HistoryEntry entry) {
-		final Map<FeatureKey<?>, Object> tfIdfs = Maps.newHashMap();
-		final int id = entry.getId();
-
-		final FPStore fpStore = FPStore.singleton();
-		final Set<Set<AtomicFeature<?>>> frequetPatterns = fpStore.getFrequetPatterns();
-
-		final AtomicFeaturesManager atomicFeaturesManager = AtomicFeaturesManager.singleton();
-		final List<AtomicFeature<?>> features = atomicFeaturesManager.getFeatures(entry);
-		final HashSet<AtomicFeature<?>> featureSet = Sets.newHashSet(features);
-		final List<Set<AtomicFeature<?>>> compoundFeatures = Lists.newLinkedList();
-
-		for (final Set<AtomicFeature<?>> fp : frequetPatterns) {
-			final boolean containsFp = featureSet.containsAll(fp);
-			if (containsFp) {
-				compoundFeatures.add(fp);
-			}
-		}
-
-		final TfIdfStore tfIdfStore = TfIdfStore.singleton();
-		for (final Set<AtomicFeature<?>> feature : compoundFeatures) {
-			final double idf = tfIdfStore.getInvertedDocumentFrequency(feature);
-			final double tf = tfIdfStore.getTermFrequency(id, feature);
-			final double tfIdf = tf * idf;
-			final TfIdfKey tfIdfKey = TfIdfKey.getKey(feature);
-			tfIdfs.put(tfIdfKey, tfIdf);
-		}
-
-		return tfIdfs;
-	}
-
-	private double cosine(final Map<FeatureKey<?>, Object> context, final Map<FeatureKey<?>, Object> features) {
-		final Set<FeatureKey<?>> contextKeySet = context.keySet();
-		final Set<FeatureKey<?>> featuresKeySet = features.keySet();
-
-		final Set<FeatureKey<?>> intersaction = Sets.newHashSet(featuresKeySet);
-		intersaction.retainAll(contextKeySet);
-
-		final double dotProduct = dotProduct(context, features, intersaction);
-
-		final Set<Entry<FeatureKey<?>, Object>> entrySet = features.entrySet();
-		double sumOfProducts = 0.0;
-		for (final Entry<FeatureKey<?>, Object> entry : entrySet) {
-			final FeatureKey<?> key = entry.getKey();
-
-			if (key instanceof VectorSpaceFeatureKey) {
-				@SuppressWarnings("rawtypes")
-				final VectorSpaceFeatureKey vectorSpaceFeatureKey = (VectorSpaceFeatureKey) key;
-				final Object value = entry.getValue();
-				@SuppressWarnings("unchecked")
-				final double product = vectorSpaceFeatureKey.multiply(value, value);
-				sumOfProducts += product;
-			}
-		}
-
-		return dotProduct / sumOfProducts;
-	}
-
-	private double dotProduct(final Map<FeatureKey<?>, Object> context, final Map<FeatureKey<?>, Object> features) {
-		final Set<FeatureKey<?>> contextKeySet = context.keySet();
-		final Set<FeatureKey<?>> featuresKeySet = features.keySet();
-
-		final Set<FeatureKey<?>> intersaction = Sets.newHashSet(featuresKeySet);
-		intersaction.retainAll(contextKeySet);
-
-		final double dotProduct = dotProduct(context, features, intersaction);
-		return dotProduct;
-	}
-
-	private double dotProduct(final Map<FeatureKey<?>, Object> context, final Map<FeatureKey<?>, Object> features, final Set<FeatureKey<?>> intersaction) {
-		double dotProduct = 0.0;
-		for (final FeatureKey<?> key : intersaction) {
-			if (key instanceof VectorSpaceFeatureKey) {
-				@SuppressWarnings("rawtypes")
-				final VectorSpaceFeatureKey vectorSpaceFeatureKey = (VectorSpaceFeatureKey) key;
-				@SuppressWarnings("unchecked")
-				final double product = vectorSpaceFeatureKey.multiply(context, features);
-				dotProduct += product;
-			}
-		}
-
-		if (dotProduct == 0.0) {
-			return 0.0;
-		}
-		return dotProduct;
-	}
-
 	private static class MessageCloseness implements Comparable<MessageCloseness> {
-		private final HistoryEntry entry;
+		private final String message;
 		private final double closeness;
 
-		public MessageCloseness(final HistoryEntry entry, final double closeness) {
-			this.entry = entry;
+		public MessageCloseness(final String message, final double closeness) {
+			this.message = message;
 			this.closeness = closeness;
 		}
 
-		public HistoryEntry getHistoryEntry() {
-			return entry;
+		public String getMessage() {
+			return message;
 		}
 
 		@Override
@@ -181,7 +147,114 @@ public class TfIdfNgramPredictionEngine extends PredictionEngine {
 
 		@Override
 		public String toString() {
-			return "{historyEntry: " + entry + ", closeness: " + closeness + "}";
+			return "{message: " + message + ", closeness: " + closeness + "}";
+		}
+	}
+
+	private static interface ClosenessAggregator extends Iterable<MessageCloseness> {
+		public void offer(final String message, final int id, final double tfIdfProduct);
+	}
+
+	private static class DotProductAggregator implements ClosenessAggregator {
+
+		private final Map<String, Double> closenesses = Maps.newHashMap();
+
+		@Override
+		public void offer(final String message, final int id, final double tfIdfProduct) {
+			final Double existing = closenesses.get(message);
+			if (existing == null) {
+				closenesses.put(message, tfIdfProduct);
+			} else {
+				final double total = existing + tfIdfProduct;
+				closenesses.put(message, total);
+			}
+		}
+
+		@Override
+		public Iterator<MessageCloseness> iterator() {
+			final Set<Entry<String, Double>> entrySet = closenesses.entrySet();
+			final Iterator<Entry<String, Double>> iterator = entrySet.iterator();
+			final Iterator<MessageCloseness> closenessIt = Iterators.transform(iterator, new Function<Entry<String, Double>, MessageCloseness>() {
+				@Override
+				public MessageCloseness apply(final Entry<String, Double> entry) {
+					final String message = entry.getKey();
+					final Double closeness = entry.getValue();
+					return new MessageCloseness(message, closeness);
+				}
+			});
+
+			return closenessIt;
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private static class ConsineAggregator implements ClosenessAggregator {
+
+		private final Table<String, Integer, Double> closenesses = HashBasedTable.create();
+
+		@Override
+		public void offer(final String message, final int id, final double tfIdfProduct) {
+			final Double existing = closenesses.get(message, id);
+			if (existing == null) {
+				closenesses.put(message, id, tfIdfProduct);
+			} else {
+				final double total = existing + tfIdfProduct;
+				closenesses.put(message, id, total);
+			}
+		}
+
+		@Override
+		public Iterator<MessageCloseness> iterator() {
+			final Map<Integer, Double> modulusCache = Maps.newHashMap();
+			final TfIdfFeatureStore tfIdfFeatureStore = TfIdfFeatureStore.singleton();
+
+			final Map<String, Map<Integer, Double>> rowMap = closenesses.rowMap();
+			final Set<Entry<String, Map<Integer, Double>>> rows = rowMap.entrySet();
+			final Iterator<Entry<String, Map<Integer, Double>>> iterator = rows.iterator();
+
+			final Iterator<MessageCloseness> closenessIt = Iterators.transform(iterator, new Function<Entry<String, Map<Integer, Double>>, MessageCloseness>() {
+				@Override
+				public MessageCloseness apply(final Entry<String, Map<Integer, Double>> entry) {
+					double closeness = 0;
+
+					final Map<Integer, Double> dotProducts = entry.getValue();
+					final Set<Entry<Integer, Double>> dotProductEntries = dotProducts.entrySet();
+					for (final Entry<Integer, Double> dotProduct : dotProductEntries) {
+						final Integer id = dotProduct.getKey();
+						Double modulus = modulusCache.get(id);
+						if (modulus == null) {
+							final Map<FeatureKey<?>, Object> features = tfIdfFeatureStore.getFeatures(id);
+							modulus = calcModulus(features);
+							modulusCache.put(id, modulus);
+						}
+
+						final Double value = dotProduct.getValue();
+						final double consine = value / modulus;
+						closeness += consine;
+					}
+
+					final String message = entry.getKey();
+					return new MessageCloseness(message, closeness);
+				}
+			});
+
+			return closenessIt;
+		}
+
+		private double calcModulus(final Map<FeatureKey<?>, Object> context) {
+			double modulus = 0.0;
+			final Set<FeatureKey<?>> keySet = context.keySet();
+			for (final FeatureKey<?> key : keySet) {
+				if (key instanceof VectorSpaceFeatureKey) {
+					@SuppressWarnings("rawtypes")
+					final VectorSpaceFeatureKey vectorSpaceFeatureKey = (VectorSpaceFeatureKey) key;
+					@SuppressWarnings("unchecked")
+					final double product = vectorSpaceFeatureKey.multiply(context, context);
+					modulus += product;
+				}
+			}
+
+			return Math.sqrt(modulus);
 		}
 	}
 }
---
+++ server/src/main/java/com/duality/server/openfirePlugin/prediction/impl/store/TfIdfFeatureStore.java
@@ -0,0 +1,86 @@
+package com.duality.server.openfirePlugin.prediction.impl.store;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.duality.server.openfirePlugin.dataTier.HistoryDatabaseAdapter;
+import com.duality.server.openfirePlugin.dataTier.HistoryEntry;
+import com.duality.server.openfirePlugin.prediction.FeatureKey;
+import com.duality.server.openfirePlugin.prediction.impl.feature.AtomicFeature;
+import com.duality.server.openfirePlugin.prediction.impl.feature.AtomicFeaturesManager;
+import com.duality.server.openfirePlugin.prediction.impl.feature.TfIdfKey;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+public class TfIdfFeatureStore {
+
+	private static final TfIdfFeatureStore INSTANCE = new TfIdfFeatureStore();
+
+	public static TfIdfFeatureStore singleton() {
+		return INSTANCE;
+	}
+
+	private final Map<Integer, Map<FeatureKey<?>, Object>> featureCache;
+
+	private TfIdfFeatureStore() {
+		featureCache = Maps.newConcurrentMap();
+	}
+
+	public Map<FeatureKey<?>, Object> getFeatures(final int historyId) {
+		final Map<FeatureKey<?>, Object> features = featureCache.get(historyId);
+		if (features != null) {
+			return features;
+		}
+
+		final HistoryDatabaseAdapter historyDbAdapter = HistoryDatabaseAdapter.singleton();
+		final HistoryEntry history = historyDbAdapter.getHistoryById(historyId);
+		return consturctFeatures(history);
+	}
+
+	public Map<FeatureKey<?>, Object> getFeatures(final HistoryEntry history) {
+		final int id = history.getId();
+		final Map<FeatureKey<?>, Object> features = featureCache.get(id);
+		if (features != null) {
+			return features;
+		}
+
+		return consturctFeatures(history);
+	}
+
+	private Map<FeatureKey<?>, Object> consturctFeatures(final HistoryEntry entry) {
+		final Map<FeatureKey<?>, Object> tfIdfs = Maps.newHashMap();
+		final int id = entry.getId();
+
+		final FPStore fpStore = FPStore.singleton();
+		final Set<Set<AtomicFeature<?>>> frequetPatterns = fpStore.getFrequetPatterns();
+
+		final AtomicFeaturesManager atomicFeaturesManager = AtomicFeaturesManager.singleton();
+		final List<AtomicFeature<?>> features = atomicFeaturesManager.getFeatures(entry);
+		final HashSet<AtomicFeature<?>> featureSet = Sets.newHashSet(features);
+		final List<Set<AtomicFeature<?>>> compoundFeatures = Lists.newLinkedList();
+
+		for (final Set<AtomicFeature<?>> fp : frequetPatterns) {
+			final boolean containsFp = featureSet.containsAll(fp);
+			if (containsFp) {
+				compoundFeatures.add(fp);
+			}
+		}
+
+		final TfIdfStore tfIdfStore = TfIdfStore.singleton();
+		for (final Set<AtomicFeature<?>> feature : compoundFeatures) {
+			final double idf = tfIdfStore.getInvertedDocumentFrequency(feature);
+			final double tf = tfIdfStore.getTermFrequency(id, feature);
+			final double tfIdf = tf * idf;
+			final TfIdfKey tfIdfKey = TfIdfKey.getKey(feature);
+			tfIdfs.put(tfIdfKey, tfIdf);
+		}
+
+		featureCache.put(id, tfIdfs);
+
+		return Collections.unmodifiableMap(tfIdfs);
+	}
+}
