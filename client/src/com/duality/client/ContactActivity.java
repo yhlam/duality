3b21ef3677e38be83f8f8c045d3c21de85675a15
kenny2tam <kenny2tam@gmail.com>
2013-05-09 01:26:39 +0800

    Avatar upload and display works on contact page

--- client/src/com/duality/client/ContactActivity.java
+++ client/src/com/duality/client/ContactActivity.java
@@ -1,7 +1,15 @@
 package com.duality.client;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.provider.ProviderManager;
+import org.jivesoftware.smackx.packet.VCard;
 
 import android.app.Activity;
 import android.content.ContentValues;
@@ -9,13 +17,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore.Images.Media;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -32,6 +46,8 @@ public class ContactActivity extends Activity{
 	private ChatDataSQL mHelper;
 	private List<String> mContactList;
 	private ContactItemAdapter mAdapter;
+	private VCard mVCard;
+	private XMPPConnection mXmpp;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,15 +57,34 @@ public class ContactActivity extends Activity{
 		startService(intent);
 	}
 	
+	
 	@Override
 	public void onStart(){
 		super.onStart();
+		mXmpp = XMPPManager.singleton().getXMPPConnection();
+		ProviderManager.getInstance().addIQProvider("vCard", "vcard-temp", new org.jivesoftware.smackx.provider.VCardProvider());
 		mHelper = new ChatDataSQL(this, DB_NAME);
 		mContactList = getContact();
 		final ListView list = (ListView) this.findViewById(R.id.contact_contactList);
 		final Button addButton = (Button) this.findViewById(R.id.contact_add);
+		final Button profileButton = (Button) this.findViewById(R.id.contact_profile);
 		mAdapter = new ContactItemAdapter(this, mContactList);
 		list.setAdapter(mAdapter);
+
+		// Display avatar
+		mVCard = new VCard();
+		try {
+			String userDomain = XMPPManager.singleton().getUsername();
+			mVCard.load(mXmpp, userDomain);
+			if(mVCard.getAvatar() != null){
+				byte[] picStream = mVCard.getAvatar();
+				Bitmap bitmap = BitmapFactory.decodeByteArray(picStream, 0, picStream.length);
+				ImageView img = (ImageView) this.findViewById(R.id.contact_image);
+				img.setImageBitmap(bitmap);
+			}
+		} catch (XMPPException e) {
+			e.printStackTrace();
+		}
 		
 		addButton.setOnClickListener(new Button.OnClickListener() {
 			@Override
@@ -76,6 +111,55 @@ public class ContactActivity extends Activity{
 				}
 			}
 		});
+		
+		profileButton.setOnClickListener(new Button.OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
+				photoPickerIntent.setType("image/*");
+				startActivityForResult(photoPickerIntent, 1);
+			}
+		});
+		
+	}
+	
+	//TODO: Change the code syntax
+	public byte[] getBytesFromBitmap(Bitmap bitmap) {
+	    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+	    bitmap.compress(CompressFormat.JPEG, 70, stream);
+	    return stream.toByteArray();
+	}
+	
+	//TODO: Change the code syntax
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+	    super.onActivityResult(requestCode, resultCode, data);
+	    if (resultCode == RESULT_OK)
+	    {
+	        Uri chosenImageUri = data.getData();
+
+	        Bitmap mBitmap = null;
+	        try {
+				mBitmap = Media.getBitmap(this.getContentResolver(), chosenImageUri);
+				ByteArrayOutputStream stream=new ByteArrayOutputStream();
+			    mBitmap.compress(Bitmap.CompressFormat.JPEG, 90, stream);
+			    byte[] result=stream.toByteArray();
+
+				VCard vCard = new VCard();
+				vCard.setAvatar(result);
+				try {
+					vCard.save(XMPPManager.singleton().getXMPPConnection());
+				} catch (XMPPException e) {
+					e.printStackTrace();
+				}
+				
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	        }
 	}
 	
 	@Override
